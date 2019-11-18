c384200682d80ce3b975d7a3592618607b4252c6
SerjTm <->
2019-11-18 04:48:04 +0300

    arena view moved to ArenaView

--- SerjTm.Sample.SudokuArena/ClientApp/src/components/Home.tsx
+++ SerjTm.Sample.SudokuArena/ClientApp/src/components/Home.tsx
@@ -1,64 +1,20 @@
 import React, { Component } from 'react';
 import * as signalR from "@aspnet/signalr";
-import { Table } from 'reactstrap';
 import { Arena } from '../models/arena';
+import { ArenaView } from '../controls/ArenaView';
 
-const displayNumber=(i:number)=> i + 1;
 
 export class Home extends Component<HomeProps> {
   static displayName = Home.name;
 
-  turn = (cell: number, number: number) => {
-    if (this.props.connection != null) {
-      this.props.connection.send("turn", { id:"f3488a67-9418-4cb1-84b6-04dd873cffaa", name: "user1" }, cell, number);
-    }
-  }
 
 
   render () {
     return (
-      <Table bordered>
-        <tbody>
-          {
-            range(9).map(row => (
-              <tr key={row}>
-                {range(9).map(col => {
-                  const cell = 9 * row + col;
-                  return <td key={col}>{this.props.arena.cells[cell] == null ? this.numberSelector(cell) : displayNumber(this.props.arena.cells[cell])}</td>
-                })
-                }
-              </tr>
-            ))
-          }
 
-        </tbody>
-      </Table>
+      <ArenaView connection={this.props.connection} arena={this.props.arena}/>
     );
   }
-  numberSelector(cell: number) {
-    return (
-      <Table borderless size="sm">
-        <tbody>
-          {
-            range(3).map(row => (
-              <tr key={row}>
-                {range(3).map(col => (
-                  <td key={col} onClick={() => this.turn(cell, 3 * row + col)}>{displayNumber(3 *row + col)}</td>
-                ))
-                }
-              </tr>
-            ))
-          }
-
-        </tbody>
-      </Table>
-   );
-  }
-}
-
-function range(n: number)
-{
-  return Array.from(Array(n).keys());
 }
 
 
