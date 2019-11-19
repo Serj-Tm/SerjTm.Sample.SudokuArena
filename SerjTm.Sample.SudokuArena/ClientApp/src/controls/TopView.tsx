ddac020db4e41fc93a75eb60bd7f9407988a3cd3
SerjTm <->
2019-11-19 22:54:26 +0300

    TopView - remove props name

--- SerjTm.Sample.SudokuArena/ClientApp/src/controls/TopView.tsx
+++ SerjTm.Sample.SudokuArena/ClientApp/src/controls/TopView.tsx
@@ -1,14 +1,14 @@
-import React, { Component, useEffect } from 'react';
+import React from 'react';
 import { Row, Col } from 'reactstrap';
 import { User_Name_Rate } from '../models/arena';
 import './TurnsView.css';
 
 
-export function TopView(props: { users: User_Name_Rate[] }) {
+export function TopView({ users }: { users: User_Name_Rate[] }) {
   return (
     <div>
       {
-        props.users.map((user, k) =>
+        users.map((user, k) =>
           <Row key={k}>
             <Col sm={{ size:2, offset: 2 }}>{user.name}</Col>
             <Col sm="1">{user.winRate}</Col>
